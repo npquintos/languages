3e16b06f3afd9220ecac279d93b36dec23e04012
Noel Quintos <npquintos@gmail.com>
2021-04-06 08:25:37 -0600

    Update Qtimer.hpp

--- Cpp/Qtimer.hpp
+++ Cpp/Qtimer.hpp
@@ -1,5 +1,24 @@
 #include <chrono>
 
+/* USAGE
+
+#include "Qtimer.hpp"
+
+int main(void)
+{
+    Qtimer tmr(7); // 7 ticks which could be in msec, sec, min, hr, to be specified later
+    tmr.start();
+    while (true) {
+        if (tmr.timed_out<hoursQ>) // here is where you specify the unit of the ticks; see last paragraph below for other "units"
+            break;  // time is up! break out of the loop.
+        if (some condition needing to re-start timer)
+            tmr.restart(); // you could use tmr.start() also because they are identical but this clarifies the intent.
+    }
+    return 0;
+ }
+ 
+ */
+
 class Qtimer
 {
     private:
